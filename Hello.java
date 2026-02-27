a758f31ce0b87d998dcafa1174831d7bdb401677
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-02-27 14:40:41 +0530

    Pretty-Patterns!

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
-public class Hello{
-    public static void main(String[] args){
-        
+public class Hello
+{
+    public static void main(String[] args)
+    {
+
         System.out.println("Yeah! like the same developer, this was my first message displayed on output:Hello World!");
     }
 }
